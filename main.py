95e05277d200c3ba73b886bfddddae7338f55413
Git-Prashanth-N <58012002+Git-Prashanth-N@users.noreply.github.com>
2019-12-04 00:01:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -58,4 +58,4 @@ def upload_file():
 # debugger for program.....................................................................
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0',debug=True)
