bb6a15e9f5141b0473f7cdea867c93b7350dc802
Git-Prashanth-N <58012002+Git-Prashanth-N@users.noreply.github.com>
2019-12-04 16:11:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -58,6 +58,6 @@ def upload_file():
 # debugger for program.....................................................................
 
 if __name__ == "__main__":
-   port = int(os.environ.get('PORT', 33507)
+   port = int(os.environ.get('PORT', 5000)
    app.run(host='0.0.0.0', port=port)
             
