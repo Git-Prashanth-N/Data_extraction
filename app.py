a70b2de57f80c7fb61237f0c2bf816de15028580
Git-Prashanth-N <58012002+Git-Prashanth-N@users.noreply.github.com>
2019-12-06 14:20:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ UPLOAD_FOLDER = './'
 
 # flask app requirements .......................................
 
-app = Flask(_name_)
+app = Flask(app)
 app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 * 1024 * 1024
