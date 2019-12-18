5bae9aa60065c2175ee1f959110c1516189da578
Git-Prashanth-N <58012002+Git-Prashanth-N@users.noreply.github.com>
2019-12-18 12:53:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+'''File name: app.py....................Author: Prashanth Noothi
+   Date created: 16/11/2019.............Date last modified: 29/12/2019
+   Python Version: 3.7 '''
+
+# importing Flask from flask....................................
+
+from flask import Flask
+
+# Upload images to the folder...................................
+
+UPLOAD_FOLDER = './'
+
+# flask app requirements .......................................
+
+app = Flask(__name__)
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
